687f4f12a3afd881b3bfa800340e2b0b0eab723a
Jinshuai <mayazureee@gmail.com>
2020-10-19 16:23:26 +1100

    Init project.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import 'antd/dist/antd.css';
+import JSONView from "./views/JSONView";
+
+
+function App() {
+  return (
+      <div className="App">
+        <JSONView/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/views/JSONView.js
@@ -0,0 +1,49 @@
+import React, {useState} from "react";
+import {Form, Button, Row, Col, Space} from 'antd'
+import TextArea from "antd/es/input/TextArea";
+import JSONTreeView from "../components/JSONTreeBuilder";
+
+export default function (props){
+
+    const [jsonStr, setJsonStr] = useState(null)
+    const [form] = Form.useForm();
+
+    const onVisualize = ()=>{
+        const value = form.getFieldValue('jsonInput')
+        setJsonStr(value)
+    }
+
+    const onFormat = ()=>{
+
+    }
+
+    return(
+        <>
+            <Row>
+                <div style={{margin:'4px'}}>
+                    <Space>
+                        <Button onClick={()=>onFormat()} disabled>format</Button>
+                        <Button type="primary" onClick={()=>onVisualize()}>visualize</Button>
+                    </Space>
+                </div>
+            </Row>
+            <Row>
+                <Col span={10}>
+                    <Form
+                        form={form}
+                    >
+                        <Form.Item
+                            name="jsonInput"
+                        >
+                            <TextArea style={{height:'100vh', resize:'none'}} placeholder={"Input your json data here"}/>
+                        </Form.Item>
+                    </Form>
+
+                </Col>
+                <Col span={14}>
+                    <JSONTreeView jsonStr={jsonStr}/>
+                </Col>
+            </Row>
+        </>
+    )
+}
